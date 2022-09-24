75f9d16a3105f67f13cb0410abe71f1b62b2f388
halimkun <ffaisalhalim@gmail.com>
2022-09-24 14:52:19 +0700

    add favicon

--- app/Views/layouts.php
+++ app/Views/layouts.php
@@ -6,6 +6,10 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title><?= isset($title) ? $title : 'Sistem Pelaporan' ?></title>
 
+    <!-- favicon -->
+    <link rel="shortcut icon" href="<?= base_url('assets/fav/favicon.ico') ?>" type="image/x-icon">
+    <link rel="icon" href="<?= base_url('assets/fav/favicon.ico') ?>" type="image/x-icon">
+
     <!-- Top Script -->
     <?= $this->include('Components/top_script'); ?>
 </head>
