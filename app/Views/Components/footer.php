d5e8e20f65d0b82c4784545f112380098bb9058b
halimkun <ffaisalhalim@gmail.com>
2022-09-24 14:52:25 +0700

    update footer

--- app/Views/Components/footer.php
+++ app/Views/Components/footer.php
@@ -1,8 +1,13 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        Created by <strong>Gito Suswanto</strong>
     </div>
     <div class="footer-right">
-
+        Copyright &copy; <span class="year"></span> <strong>Stisla</strong> <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
     </div>
 </footer>
+<script>
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementsByClassName("year")[0].innerHTML = n;
+</script>
