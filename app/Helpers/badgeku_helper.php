0520be0e01e48da3bc3b8d6181546af23be4e726
halimkun <ffaisalhalim@gmail.com>
2023-02-17 08:26:52 +0700

    themes by user

--- app/Helpers/badgeku_helper.php
+++ app/Helpers/badgeku_helper.php
@@ -1,9 +1,11 @@
 <?php 
     function badgeGen($roles) {
         if ($roles == "admin") {
-            return "<span class='badge badge-primary'>".$roles."</span>";
+            return "<span class='badge badge-".userColor()."'>".$roles."</span>";
         } else if ($roles == "pengguna") {
             return "<span class='badge badge-dark'>".$roles."</span>";
+        } else if ($roles == "petugas") {
+            return "<span class='badge badge-success'>".$roles."</span>";
         } else {
             return "<span class='badge badge-danger'>".$roles == "" ? "undefined" : $roles."</span>";
         }
--- app/Helpers/userku_helper.php
+++ app/Helpers/userku_helper.php
@@ -1,6 +1,28 @@
-<?php 
-    function getUserName($id) {
-        $user = new \App\Models\UserModel();
-        $user = $user->find($id);
-        return $user->nama;
+<?php
+function getUserName($id)
+{
+    $user = new \App\Models\UserModel();
+    $user = $user->find($id);
+    return $user->nama;
+}
+
+// user themes
+function userColor()
+{
+    if (logged_in()) {
+        $role = user()->getRoles();
+        if (in_array('admin', $role)) {
+            $t = 'primary';
+        } else if (in_array('petugas', $role)) {
+            $t = 'info';
+        } else if (in_array('pengguna', $role)) {
+            $t = 'success';
+        } else {
+            $t = 'danger';
+        }
+
+        return $t;
+    } else {
+        return '';
     }
+}
