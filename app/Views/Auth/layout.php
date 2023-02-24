28c2b9a43a6cd2d1484454849f94780b1c936e3c
gito <gitouye1@gmail.com>
2023-02-24 10:46:30 +0700

    Update layout.php

--- app/Views/Auth/layout.php
+++ app/Views/Auth/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Aduan Kepolisian</title>
+    <title>Sistem Pelaporan</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="<?= base_url('assets/modules/bootstrap/css/bootstrap.min.css') ?>">
